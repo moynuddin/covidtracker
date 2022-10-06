594007dcfef94737699b1055e233d3b54e4e4e7f
Moin uddin <moin_uddin@mohawkind.com>
2022-10-06 21:47:24 +0530

    g analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { DiseaseService } from './_services/disease.service';
+import { filter } from 'rxjs/operators';
+
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +12,7 @@ import { DiseaseService } from './_services/disease.service';
 export class AppComponent implements OnInit {
   title = 'covid-tracker';
   map: any;
-  constructor(private diseaseService: DiseaseService) {}
+  constructor(private router: Router, private diseaseService: DiseaseService) {}
 
   ngOnInit() {
     this.diseaseService.chartState.subscribe(
@@ -19,5 +23,16 @@ export class AppComponent implements OnInit {
         console.log(error);
       }
     );
+    this.setUpAnalytics();
+  }
+  setUpAnalytics() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        console.log(event);
+        gtag('config', 'G-0RWDK55DLV', {
+          page_path: event.urlAfterRedirects,
+        });
+      });
   }
 }
